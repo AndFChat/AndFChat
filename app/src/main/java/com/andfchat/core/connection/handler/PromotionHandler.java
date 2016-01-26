0ecda4ba97f09413d1922b1f736c07657456eaf5
Pandora <kiarahexen@outlook.com>
2016-01-26 00:12:54 -0600

    Promotion and mode setting working and tested
    Both /cop and /setmode now are tested and working.

--- app/src/main/java/com/andfchat/core/connection/handler/PromotionHandler.java
+++ app/src/main/java/com/andfchat/core/connection/handler/PromotionHandler.java
@@ -38,11 +38,13 @@ public class PromotionHandler extends TokenHandler {
         if (token == ServerToken.COA) {
             JSONObject json = new JSONObject(msg);
             String channel = json.getString("channel");
-            String mode = json.getString("character");
+            String character = json.getString("character");
 
             Chatroom chatroom = chatroomManager.getChatroom(channel);
 
             if (chatroom != null) {
+                ChatEntry entry = entryFactory.getNotation(characterManager.findCharacter(character), " has been promoted in " + chatroom.getName() + ".");
+                this.addChatEntryToActiveChat(entry);
             }
             else {
                 Ln.e("Promotion is for a unknown channel: " + channel);
